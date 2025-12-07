0ed0d5862aa228d88487ce0510a5d8b67d610ce7
Mukund Shelake <mukundshelake400@gmail.com>
2025-12-07 13:50:59 -0500

    Tracking large SF files via lfs

---
+++ SFs/sfPaths.py
@@ -0,0 +1,212 @@
+SFs = {
+# HLT scale factors
+## Electron
+### UL2016preVFP
+#### Tight
+    "UL2016preVFP_el_HLT_Tight" : {
+        "fileName": "UL2016preVFP_el_HLT_Tight.root",
+        "publicHash": "gX4Z3PjnOSBvI3c",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+#### Veto
+    "UL2016preVFP_el_HLT_Veto" : {
+        "fileName": "UL2016preVFP_el_HLT_Veto.root",
+        "publicHash": "sfgQhZrcrQmqceR",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2016postVFP
+#### Tight
+    "UL2016postVFP_el_HLT_Tight" : {
+        "fileName": "UL2016postVFP_el_HLT_Tight.root",
+        "publicHash": "qfSt4p3vvYMWznn",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+#### Veto
+    "UL2016postVFP_el_HLT_Veto" : {
+        "fileName": "UL2016postVFP_el_HLT_Veto.root",
+        "publicHash": "5ZERLfAssBxELOd",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2017
+#### Tight
+    "UL2017_el_HLT_Tight" : {
+        "fileName": "UL2017_el_HLT_Tight.root",
+        "publicHash": "CwbkDFuQri9EsDw",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+#### Veto
+    "UL2017_el_HLT_Veto" : {
+        "fileName": "UL2017_el_HLT_Veto.root",
+        "publicHash": "7gnGHwzcIDlFG2c",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2018
+#### Tight
+    "UL2018_el_HLT_Tight" : {
+        "fileName": "UL2018_el_HLT_Tight.root",
+        "publicHash": "51hfYk7cyiV9mUy",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+#### Veto
+    "UL2018_el_HLT_Veto" : {
+        "fileName": "UL2018_el_HLT_Veto.root",
+        "publicHash": "OKF3LMf9prrvBEb",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+## Muon
+### UL2016preVFP
+    "UL2016preVFP_mu_HLT" : {
+        "fileName": "UL2016preVFP_mu_HLT.json",
+        "publicHash": "X7SFhqIK0eZv4c0",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2016postVFP
+    "UL2016postVFP_mu_HLT" : {
+        "fileName": "UL2016postVFP_mu_HLT.json",
+        "publicHash": "PdKNVcerSCMQCqD",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2017
+    "UL2017_mu_HLT" : {
+        "fileName": "UL2017_mu_HLT.json",
+        "publicHash": "0JUlOd4L6lWB14F",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2018
+    "UL2018_mu_HLT" : {
+        "fileName": "UL2018_mu_HLT.json",
+        "publicHash": "DckK28ELWE6LX3V",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+# ID scale factors
+## Electron
+### UL2016preVFP
+    "UL2016preVFP_el_ID" : {
+        "fileName": "UL2016preVFP_el_ID.json", 
+        "publicHash": "x23y01GJgobGoIY",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2016postVFP
+    "UL2016postVFP_el_ID" : {
+        "fileName": "UL2016postVFP_el_ID.json", 
+        "publicHash": "5PKdyuP1L1lxinx",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2017
+    "UL2017_el_ID" : {
+        "fileName": "UL2017_el_ID.json", 
+        "publicHash": "iVo7ilQ6wmCHccL",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2018
+    "UL2018_el_ID" : {
+        "fileName": "UL2018_el_ID.json", 
+        "publicHash": "05Vhs88pUG4IK1F",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+## Muon
+### UL2016preVFP
+    "UL2016preVFP_mu_ID" : {
+        "fileName": "UL2016preVFP_mu_ID.json", 
+        "publicHash": "PK3ZqRT1d73n5An",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2016postVFP
+    "UL2016postVFP_mu_ID" : {
+        "fileName": "UL2016postVFP_mu_ID.json", 
+        "publicHash": "dxtbcDEKNL4OiPd",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2017
+    "UL2017_mu_ID" : {
+        "fileName": "UL2017_mu_ID.json", 
+        "publicHash": "j3TyH1zfLUJjuRr",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2018
+    "UL2018_mu_ID" : {
+        "fileName": "UL2018_mu_ID.json", 
+        "publicHash": "6SpFGME2gIRIet8",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+# B-Tagging scale factors
+## Electron + Muon
+### UL2016preVFP
+    "UL2016preVFP_jet_Btagging" : {
+        "fileName": "UL2016preVFP_jet_Btagging.json", 
+        "publicHash": "blm4W9q6MjsXdgn",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2016postVFP
+    "UL2016postVFP_jet_Btagging" : {
+        "fileName": "UL2016postVFP_jet_Btagging.json", 
+        "publicHash": "uXJIBljhmMDgAZw",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2017
+    "UL2017_jet_Btagging" : {
+        "fileName": "UL2017_jet_Btagging.json", 
+        "publicHash": "XfCuAh9PC7D4FSu",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2018
+    "UL2018_jet_Btagging" : {
+        "fileName": "UL2018_jet_Btagging.json", 
+        "publicHash": "xCmGMvGb2iU36Pp",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+# Jet Jerc
+## Electron + Muon
+### UL2016preVFP
+    "UL2016preVFP_jet_Jerc" : {
+        "fileName": "UL2016preVFP_jet_Jerc.json", 
+        "publicHash": "IZhleUEmmHQ4BnO",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2016postVFP
+    "UL2016postVFP_jet_Jerc" : {
+        "fileName": "UL2016postVFP_jet_Jerc.json", 
+        "publicHash": "Md4fxXC1i8eBJdz",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2017
+    "UL2017_jet_Jerc" : {
+        "fileName": "UL2017_jet_Jerc.json", 
+        "publicHash": "EnZ5mnWTWMimsam",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },
+### UL2018
+    "UL2018_jet_Jerc" : {
+        "fileName": "UL2018_jet_Jerc.json", 
+        "publicHash": "sSUzikoGDYyh7cu",
+        "srcPath": "coffeaDependencies/SFs",
+        "targetPath": "./SFs"
+        },       
+}
